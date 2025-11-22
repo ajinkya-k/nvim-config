a936491f78f60774121eaee1a98d414c6686c4b9
Ajinkya Kokandakar <ajinkyakokandakar@gmail.com>
2025-11-22 00:45:58 -0800

    removed catpuccin theme

--- lua/plugins/colorthemes.lua
+++ lua/plugins/colorthemes.lua
@@ -1,19 +1,4 @@
 return {
-   {
-    'catppuccin/nvim',
-    name = 'catppuccin',
-    enabled = false,
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      -- set colorscheme and overwrite highlights
-      vim.cmd.colorscheme 'catppuccin-macchiato'
-      local colors = require 'catppuccin.palettes.macchiato'
-      vim.api.nvim_set_hl(0, 'Tabline', { fg = colors.green, bg = colors.mantle })
-      vim.api.nvim_set_hl(0, 'TermCursor', { fg = '#A6E3A1', bg = '#A6E3A1' })
-    end,
-  },
-
   {
     'oxfist/night-owl.nvim',
     enabled = false,
